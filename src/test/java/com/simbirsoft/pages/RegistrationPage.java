f2f410399fc05daaed935c69e20b32a085df4415
etonin97 <tazcool@yandex.ru>
2024-05-17 13:53:20 +0400

    выполнение U3, U4

--- src/test/java/com/simbirsoft/pages/RegistrationPage.java
+++ src/test/java/com/simbirsoft/pages/RegistrationPage.java
@@ -1,8 +1,7 @@
 package com.simbirsoft.pages;
 
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -10,8 +9,12 @@ public class RegistrationPage {
 
     private static final String USER_NAME_ID = "username";
     private static final String PASSWORD_ID = "password";
-    private static final String USER_NAME_DESCRIPTION_ID = "formly_1_input_username_0";
+    private static final String USER_NAME_DESCRIPTION_CSS =
+            "input.form-control.ng-pristine.ng-untouched.ng-invalid.ng-invalid-required.ng-valid-maxlength.ng-valid-minlength";
+    private static final String USER_NAME_DESCRIPTION_WRONG_CSS =
+            "input.form-control.ng-valid-maxlength.ng-dirty.ng-valid-parse.ng-valid-required.ng-valid.ng-valid-minlength.ng-touched";
     private static final String LOGIN_BUTTON_CSS = "button.btn.btn-danger";
+    private static final String INCORRECT_USERNAME_OR_PASSWORD_XPATH = "/html/body/div[1]/div/div/div/div[2]";
 
     private final WebDriver driver;
     public RegistrationPage(WebDriver driver) {
@@ -25,11 +28,11 @@ public class RegistrationPage {
     @FindBy(id = PASSWORD_ID)
     private WebElement password;
 
-    @FindBy(id = USER_NAME_DESCRIPTION_ID)
-    private WebElement userNameDescription;
-
     @FindBy(css = LOGIN_BUTTON_CSS)
     private WebElement loginButton;
+
+    @FindBy(xpath = INCORRECT_USERNAME_OR_PASSWORD_XPATH)
+    private WebElement incorrectLoginOrPassword;
 
     @Step("Найти строку \"UserName\"")
     public WebElement getUserName() {
@@ -43,7 +46,11 @@ public class RegistrationPage {
 
     @Step("Найти строку \"UserNameDescription\"")
     public WebElement getUserNameDescription() {
-        return userNameDescription;
+        try {
+            return driver.findElement(By.cssSelector(USER_NAME_DESCRIPTION_CSS));
+        } catch (NoSuchElementException exc) {
+            return driver.findElement(By.cssSelector(USER_NAME_DESCRIPTION_WRONG_CSS));
+        }
     }
 
     @Step("Найти кнопку входа")
@@ -52,24 +59,27 @@ public class RegistrationPage {
     }
 
     @Step("Ввести имя пользователя")
-    public void setUserName(String value) {
+    public void setUserName(CharSequence value) {
         getUserName().sendKeys(value);
     }
 
     @Step("Ввести пароль пользователя")
-    public void setPassword(String value) {
+    public void setPassword(CharSequence value) {
         getPassword().sendKeys(value);
     }
 
     @Step("Ввести описание имени пользователя")
-    public void setUserNameDescription(String value) {
+    public void setUserNameDescription(CharSequence value) {
         getUserNameDescription().sendKeys(value);
     }
 
     @Step("Ввести форму ввода")
     public RegistrationPage fillForm(String username, String password, String description) {
+        setUserName(Keys.chord(Keys.CONTROL, "a"));
         setUserName(username);
+        setPassword(Keys.chord(Keys.CONTROL, "a"));
         setPassword(password);
+        setUserNameDescription(Keys.chord(Keys.CONTROL, "a"));
         setUserNameDescription(description);
         return this;
     }
@@ -77,6 +87,11 @@ public class RegistrationPage {
     @Step("Кликнуть кнопку входа")
     public void clickLoginButton(){
         getLoginButton().click();
+    }
+
+    @Step("Найти строку некорректного логина или пароля")
+    public WebElement getIncorrectLoginOrPassword() {
+        return incorrectLoginOrPassword;
     }
 
 }
---
+++ src/test/java/com/simbirsoft/tests/DateProviderBaseTest.java
@@ -0,0 +1,115 @@
+package com.simbirsoft.tests;
+
+import com.simbirsoft.BaseTest;
+import com.simbirsoft.config.ConfProperties;
+import com.simbirsoft.pages.RegistrationPage;
+import com.simbirsoft.pages.SuccessfulRegPage;
+import io.qameta.allure.*;
+import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+
+public class DateProviderBaseTest extends BaseTest {
+
+    protected static RegistrationPage registrationPage;
+    protected static SuccessfulRegPage successfulRegPage;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(ConfProperties.getProperty("registrationPage"));
+
+        registrationPage = new RegistrationPage(driver);
+        successfulRegPage = new SuccessfulRegPage(driver);
+    }
+
+    @DataProvider(name = "data-provider")
+    public Object[][] dataProviderMethod() {
+        return new Object[][] { { "angular", "password", "descr"},
+                { "UsernameWrong_1", "password", "descrAnother_1" },
+                {"angular", "passwordWrong_2", "descrAnother_2"},
+                {"UsernameWrong_3", "passwordWrong_3", "descrAnother_3"}};
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic("Проверка сайта \"Way2Automation\"")
+    @Feature("Авторизация")
+    @Story("Проверка авторизации пользователя \"userName\"")
+    @Test(dataProvider = "data-provider")
+    public void test(String name, String pass, String desc) {
+        SoftAssertions softAssertions = new SoftAssertions();
+        registrationPage.fillForm(
+                name,
+                pass,
+                desc
+        ).clickLoginButton();
+
+        if (name.equals(ConfProperties.getProperty("userName")) && pass.equals(ConfProperties.getProperty("password"))) {
+            WebElement resultLogged = successfulRegPage.getLoggedIn();
+
+            softAssertions.assertThat(resultLogged).isNotNull();
+
+            successfulRegPage.clickLogoutButton();
+        } else {
+            WebElement incorrectLoginOrPassword = registrationPage.getIncorrectLoginOrPassword();
+            softAssertions.assertThat(incorrectLoginOrPassword).isNotNull();
+        }
+    }
+
+    @DataProvider(name = "failure-data-provider")
+    public Object[][] dataProviderFailureMethod() {
+        return new Object[][] { { "", "", "descr"},
+                { "angular", "password", "" }};
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic("Проверка сайта \"Way2Automation\"")
+    @Feature("Авторизация")
+    @Story("Проверка авторизации пользователя без ввода данных")
+    @Test(dataProvider = "failure-data-provider")
+    public void testFailed(String name, String pass, String desc) {
+        SoftAssertions softAssertions = new SoftAssertions();
+        registrationPage.fillForm(name, pass, desc).clickLoginButton();
+
+        WebElement resultLogged = successfulRegPage.getLoggedIn();
+        softAssertions.assertThat(resultLogged).isNotNull();
+        successfulRegPage.clickLogoutButton();
+    }
+
+    @AfterMethod
+    public void onTestFailure(ITestResult tr) throws IOException {
+        if (!tr.isSuccess()) {
+            makeScreenshot();
+        }
+    }
+
+    @Attachment(value = "Attachment Screenshot", type = "image/png")
+    public byte[] makeScreenshot() throws IOException {
+        Screenshot screenshot = new AShot()
+                .shootingStrategy(ShootingStrategies.viewportPasting(100))
+                .takeScreenshot(driver);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(screenshot.getImage(), "jpg", baos);
+        return baos.toByteArray();
+    }
+
+    @AfterClass
+    public static void exit() {
+        driver.quit();
+    }
+}
